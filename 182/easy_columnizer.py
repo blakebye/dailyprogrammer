9c8eeb72eba5ceb253685c3f0dfb40ba3652530e
Blake Bye <blake.bye@gmail.com>
2014-09-30 01:00:49 -0700

    clean up some functions

--- 182/easy_columnizer.py
+++ 182/easy_columnizer.py
@@ -4,61 +4,82 @@ into columns based on what the user would like.
 """
 
 import math
-import sys
 
-def get_file():
-    if len(sys.argv) == 2:
-        f = open(sys.argv[1])
-    else:
-        print "easy_columnizer.py: no file given to columnize"
-        f = raw_input("What is the name of the file you wish to columnize?\n")
-        f = open(f)
-    return f
+INPUT_FILE = "c182e-input.txt"
 
-def columnize():
-    f = get_file()
-    number, width, space = [int(x) for x in f.readline().split()]
+
+def line_to_lines(line, width):
+    """
+    This function takes one line of text, breaks it into words, and
+    fills new lines that will take exactly "width" from as many words
+    as possible and then pad with spaces to match width.
+    """
     list_of_lines = list()
-    for line in f:
-        words = line.split()
-        if len(line) > width:
-            line_length = -1 # offsets the added +1 from the last space
-            this_line = str()
-            for word in words:
-                line_length += len(word) + 1
-                # there is space for that word in this line
-                if line_length <= width:
-                    this_line += str(word) + " "
-                # there is not space
-                else:
-                    # take back the last word, not putting it here
-                    line_length -= (len(word) + 1)
-                    # fill out the width with spaces
-                    while line_length < (width + space):
-                        this_line += " "
-                        line_length += 1
-                    # put the line we've been working on in the list
-                    list_of_lines.append(this_line)
-                    # start a new line with the word we're on
-                    this_line = str(word) + " "
-                    line_length = len(word)
-            while line_length < (width + space):
-                        this_line += " "
-                        line_length += 1
-            list_of_lines.append(this_line)
-    list_of_lines = [''.join(list(line)[:-1]) for line in list_of_lines]
-    # list_of_lines is missing last line
+    words = line.split()
+    if len(line) > width:
+        # offsets the added +1 from the last space
+        line_length = -1
+        this_line = str()
+        for word in words:
+            line_length += len(word) + 1
+            # there is space for that word in this line
+            if line_length < width:
+                this_line += str(word) + " "
+            # there is not space
+            else:
+                # take back the last word, not putting it here
+                line_length -= (len(word) + 1)
+                # fill out the width with spaces
+                while line_length < width:
+                    this_line += " "
+                    line_length += 1
+                # put the line we've been working on in the list
+                list_of_lines.append(this_line)
+                # start a new line with the word we're on
+                this_line = str(word) + " "
+                line_length = len(word)
+        while line_length < width:
+            this_line += " "
+            line_length += 1
+        list_of_lines.append(this_line)
+    # extra spaces are consistent at the end of lines, too lazy to find why
+    list_of_lines = ["".join(list(line)[:-2]) for line in list_of_lines]
+    return list_of_lines
+
+
+def columnize(inp):
+    """
+    This funcion takes a filename and works it's columnizing magic.
+    """
+    # the first line is always the configuration for the layout
+    number, width, space = [int(x) for x in inp.readline().split()]
+    # this will become a list of lines of proper width
+    list_of_lines = list()
+    for line in inp:
+        list_of_lines.extend(line_to_lines(line, width))
+    # this ensures the columns are of similar lengths and not cut-off
     max_rows = int(math.ceil(len(list_of_lines) / float(number)))
-    # row length is perfect, no lines cut off
-    new_list = list()
-    # our new ROW LIST is empty
+    # this will be our text in columns
+    columns = list()
     for old_location, line in enumerate(list_of_lines):
         if old_location < max_rows:
-            new_list.append(line)
+            # just move over the first column
+            columns.append(line)
         else:
+            # these columns are shifted up and spaced appropriately
             new_location = old_location % max_rows
-            new_list[new_location] += " " + str(line)
-    for line in new_list:
-        print line
+            columns[new_location] += " " * space + str(line)
+    return columns
 
-columnize()
+
+def main():
+    """
+    This function displays the solution for Lorem ipsum
+    """
+    inp = open(INPUT_FILE)
+    columns = columnize(inp)
+    for column in columns:
+        print column
+
+if __name__ == "__main__":
+    main()
